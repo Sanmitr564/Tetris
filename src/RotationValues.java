0202cf8ea1b7b58e2d94d3960290199f63697ed9
Sanmitr564 <generic39542@gmail.com>
2022-03-23 18:32:26 -0700

    J & L Rotation Values

--- src/RotationValues.java
+++ src/RotationValues.java
@@ -1,6 +1,9 @@
 public class RotationValues {
-    //Coordinates are listed as {yOffset, xoffset}
-    //use Global.X and Global.Y to grab the correct values
+    //Coordinates are listed as {rowOffset, colOffset}/{yOffset, xOffset}
+    //All values are positions relative to the center of rotation of the tetromino
+    //Arrays are called as [facingDirection][blockNumber][row/colOffset]
+    //For blocks that rotate around a corner, (0,0) is the block to the bottom left of the corner
+
     public static int[][][] jValues = new int[][][]{
             {
                     {1, -1},
@@ -9,19 +12,49 @@ public class RotationValues {
                     {0, 1}
             },
             {
-                    {1,1},
-                    {1,0},
-                    {0,0},
-                    {-1,0}
+                    {1, 1},
+                    {1, 0},
+                    {0, 0},
+                    {-1, 0}
             },
             {
-                    {1,-1},
-                    {0,1},
-                    {0,0},
-                    {0,-1}
+                    {-1, 1},
+                    {0, 1},
+                    {0, 0},
+                    {0, -1}
             },
             {
-                    {-1,-1}
+                    {-1, -1},
+                    {-1, 0},
+                    {0, 0},
+                    {1, 0}
+            }
+    };
+
+    public static int[][][] lValues = new int[][][]{
+            {
+                    {0, -1},
+                    {0, 0},
+                    {0, 1},
+                    {1, 1}
+            },
+            {
+                    {1, 0},
+                    {0, 0},
+                    {-1, 0},
+                    {-1, 1}
+            },
+            {
+                    {0, 1},
+                    {0, 0},
+                    {0, -1},
+                    {-1, -1}
+            },
+            {
+                    {1, -1},
+                    {1, 0},
+                    {0, 0},
+                    {-1, 0}
             }
     };
 }
