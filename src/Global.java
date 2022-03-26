30df2af026ba49dd588d45bb6385542a76e10407
Sanmitr564 <generic39542@gmail.com>
2022-03-26 15:55:36 -0700

    Smaller Field

--- src/Global.java
+++ src/Global.java
@@ -7,9 +7,18 @@ public class Global {
     public static final int X = 1;
     public static final int Y = 0;
 
+    public static final int FIELD_WIDTH = 400;
+    public static final int FIELD_HEIGHT = 800;
+
+    //Bottom Left Corner
+    public static final int FIELD_X = 50;
+    public static final int FIELD_Y = 100;
+
     public static final int ROWS = 23;
+
+    public static final int VISIBLE_ROWS = 20;
     public static final int COLS = 10;
-    public static final float SQUARE_SIZE = 48;
+    public static final float SQUARE_SIZE = FIELD_WIDTH/(float)COLS - 2;
 
     public static final Color[] COLORS = new Color[] {
             Color.BLUE,
--- src/Tetris.java
+++ src/Tetris.java
@@ -1,6 +1,5 @@
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.utils.viewport.*;
@@ -9,17 +8,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Intersector;
-import com.badlogic.gdx.graphics.g2d.GlyphLayout;
-import com.badlogic.gdx.utils.*;
-import com.badlogic.gdx.*;
 
-import java.security.Key;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -51,7 +41,7 @@ public class Tetris extends ApplicationAdapter
         font = new BitmapFont();
         batch = new SpriteBatch();//if you want to use images instead of using ShapeRenderer 
 
-        board = new Color[Global.ROWS][Global.COLS];
+        board = new Color[Global.VISIBLE_ROWS][Global.COLS];
         for (Color[] colors : board) {
             Arrays.fill(colors, Color.WHITE);
         }
@@ -89,6 +79,7 @@ public class Tetris extends ApplicationAdapter
 
         drawGrid();
     }
+
     @Override
     public void resize(int width, int height){
         viewport.update(width, height, true);
@@ -143,10 +134,10 @@ public class Tetris extends ApplicationAdapter
     private void drawGrid() {
         renderer.begin(ShapeType.Filled);
 
-        for (int y = 0; y < Global.ROWS; y++) {
+        for (int y = 0; y < Global.VISIBLE_ROWS; y++) {
             for (int x = 0; x < Global.COLS; x++) {
                 renderer.setColor(board[y][x]);
-                renderer.rect(1 + x * (Global.SQUARE_SIZE + 2), 1 + y * (Global.SQUARE_SIZE + 2), Global.SQUARE_SIZE, Global.SQUARE_SIZE);
+                renderer.rect(Global.FIELD_X + 1 + x * (Global.SQUARE_SIZE + 2), 100 + 1 + y * (Global.SQUARE_SIZE + 2), Global.SQUARE_SIZE, Global.SQUARE_SIZE);
             }
         }
         renderer.end();
