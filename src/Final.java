d0232122a496ea9d90d528b6b671879b6048ae95
Sanmitr564 <generic39542@gmail.com>
2022-05-02 09:10:36 -0700

    Remove Unused Imports

--- src/Final.java
+++ src/Final.java
@@ -1,8 +1,6 @@
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Final {
     public static final int WORLD_WIDTH = 1920;
